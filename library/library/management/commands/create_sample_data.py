eb44c51c49a8a49179f37cc0bc2b7f032c5bf332
Rubayet Shareen <70822968+Rubayet09@users.noreply.github.com>
2025-01-03 17:58:02 +0600

    Update create_sample_data.py

--- library/library/management/commands/create_sample_data.py
+++ library/library/management/commands/create_sample_data.py
@@ -1,41 +1,3 @@
-# from django.core.management.base import BaseCommand
-# from library.models import Author, Publisher, Book
-# from datetime import date
-
-# class Command(BaseCommand):
-
-#     def handle(self, *args, **kwargs):
-#         # Create author
-#         author = Author.objects.create(
-#             name='Humayun Ahmed',
-#             email='humayun@gmail.com',
-#             bio='A great Bangladeshi author'
-#         )
-#         self.stdout.write(self.style.SUCCESS('Created author'))
-
-#         # Create publisher
-#         publisher = Publisher.objects.create(
-#             name='Anondo Publisher',
-#             address='Bangla Motor',
-#             website='http://anondo-publication-ltd.com'
-#         )
-#         self.stdout.write(self.style.SUCCESS('Created publisher'))
-
-#         # Create books
-#         for i in range(10):
-#             Book.objects.create(
-#                 book_id=f'BOOK{i+1}',
-#                 title=f'Book Title {i+1}',
-#                 author=author,
-#                 publisher=publisher,
-#                 publication_date=date.today(),
-#                 isbn=f'123333{i}',
-#                 available=True
-#             )
-        
-#         self.stdout.write(self.style.SUCCESS('Created 10 books'))
-
-
 from django.core.management.base import BaseCommand
 from library.models import Author, Publisher, Book
 from datetime import date
@@ -43,17 +5,14 @@ from random import randint, choice
 
 
 class Command(BaseCommand):
-    help = 'Creates sample data for illustrating select_related and prefetch_related'
 
     def handle(self, *args, **kwargs):
-        # Clear existing data
         Book.objects.all().delete()
         Author.objects.all().delete()
         Publisher.objects.all().delete()
 
         self.stdout.write(self.style.WARNING('Cleared existing data'))
 
-        # Create publishers
         publishers = []
         for i in range(5):
             publisher = Publisher.objects.create(
@@ -64,7 +23,6 @@ class Command(BaseCommand):
             publishers.append(publisher)
         self.stdout.write(self.style.SUCCESS('Created 5 publishers'))
 
-        # Create authors
         authors = []
         for i in range(10):
             author = Author.objects.create(
@@ -75,7 +33,6 @@ class Command(BaseCommand):
             authors.append(author)
         self.stdout.write(self.style.SUCCESS('Created 10 authors'))
 
-        # Create books
         for i in range(50):
             book = Book.objects.create(
                 book_id=f'BOOK{i+1}',
